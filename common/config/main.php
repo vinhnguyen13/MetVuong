4105a2944075e9504c7dc8a3c9ae28b737db782a
vinh.nguyen <quangvinh.nguyen@trungthuygroup.vn>
2015-10-01 11:41:56 +0700

    console, user acl

--- common/config/main.php
+++ common/config/main.php
@@ -28,7 +28,7 @@ return [
             'controllerMap' => [
                 'assignment' => [
                     'class' => 'mdm\admin\controllers\AssignmentController',
-                    'userClassName' => 'common\models\User', // fully qualified class name of your User model
+                    'userClassName' => 'dektrium\user\models\User', // fully qualified class name of your User model
                     // Usually you don't need to specify it explicitly, since the module will detect it automatically
                     'idField' => 'id',        // id field of your User model that corresponds to Yii::$app->user->id
                     'usernameField' => 'username', // username field of your User model
--- console/index.php
+++ console/index.php
@@ -8,7 +8,10 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
 require(dirname(dirname(__FILE__)) . '/vendor/yiisoft/yii2/Yii.php');
-$config = require(__DIR__ . '/config/main.php');
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/config/main.php'),
+    require(__DIR__ . '/config/main-local.php')
+);
 
 $application = new yii\console\Application($config);
 $exitCode = $application->run();
